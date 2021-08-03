3807008009948ed91d347632d4f41827f6dd3955
Vadim Bukstein <vbukstein@gmail.com>
2021-08-03 08:39:52 +0300

    Test

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Vadim" );
+        System.out.println( "Hello World! Vadim Test_03082021" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
