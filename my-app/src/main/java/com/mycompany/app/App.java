e2aa03ccfacc07b5dbac76ae036d5282dce683c8
vbukstein <57598959+vbukstein@users.noreply.github.com>
2021-08-05 13:45:38 +0300

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Vadim Bukstein" );
+        System.out.println( "Hello World! Vadim Bukstein test" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
