0ba855ce526932a408a88639a79afe5e4d217702
serjoze <serjo.z@gmail.com>
2022-01-26 00:43:28 +0200

    log-200

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -15,7 +15,7 @@ public class App
         /**
          * Checker prevents this from compiling...
          */
-//        System.out.println("myObject: " + myObject.toString());
+       // System.out.println("myObject: " + myObject.toString());
         /**
          * ... which is simply fantastic. It shows:
          *
