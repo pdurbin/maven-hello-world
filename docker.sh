36954631cc233f2b4fc6eccbbfe26fa7e2c9d0e8
Michaaliev <Michaaliev1663@gmail.com@gmail.com>
2022-11-27 16:20:08 +0000

    version update

--- docker.sh
+++ docker.sh
@@ -1,2 +1,1 @@
-docker run --name hello_world -dit michaaliev/hello_world:1.0.20 sh
-docker run --name hello_world -dit michaaliev/hello_world:1.0.21 sh
+docker run --name hello_world -dit michaaliev/hello_world:1.0.22 sh
