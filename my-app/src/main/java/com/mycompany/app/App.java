55d21a27f1482e5c3aa0e9d301970cbc6dff78c5
rj185092 <69118674+rj185092@users.noreply.github.com>
2023-01-29 21:50:19 +0200

    changing version and adding naming into code

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! My name Is Raanan Jovani" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
